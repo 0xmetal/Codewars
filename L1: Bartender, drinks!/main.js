231658a36b40768dfe18d6f0347c9b5d30813c94
metlx <metlx>
2022-05-10 13:42:58 -0400

    solution for challenge

---
+++ L1: Bartender, drinks!/main.js
@@ -0,0 +1,11 @@
+getDrinkByProfession = O => {
+  o = O.toLowerCase()
+  return o == 'jabroni' ? 'Patron Tequila'
+  : o == 'school counselor' ? 'Anything with Alcohol'
+  : o == 'programmer' ? 'Hipster Craft Beer'
+  : o == 'bike gang member' ? 'Moonshine'
+  : o == 'politician' ? 'Your tax dollars'
+  : o == 'rapper' ? 'Cristal'
+  : o == 'pundit' ? 'Beer'
+  : o == 'pug' ? 'Beer' : 'Beer'
+}
