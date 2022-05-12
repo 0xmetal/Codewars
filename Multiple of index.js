7a65f4d23d302811acf1d2526218f76681482d10
metlx <metlx>
2022-05-11 22:46:31 -0400

    solution for challenge

---
+++ Multiple of index.js
@@ -0,0 +1,11 @@
+// day 1
+
+function multipleOfIndex(arr) {
+  let newarr = []
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] % i == 0) {
+      newarr.push(arr[i])
+    }
+  }
+  return newarr
+}
