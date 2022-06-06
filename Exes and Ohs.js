f1083a1891fdee9ccf1c2b288d35a5d7eb47e4f0
metlx <metlx>
2022-06-05 23:04:42 -0400

    solution for challenge

--- Exes and Ohs.js
+++ Exes and Ohs.js
@@ -1,6 +1,4 @@
 function XO(str){
-  // Check to see if a string
-  // has the same amount of 'x's and 'o's
   let o = 0, x = 0
   for (let i = 0; i < str.length; i++){
     if (str[i].toLowerCase() == 'o'){
