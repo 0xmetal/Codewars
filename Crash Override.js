1d095a3a5049a2633f9f3b58aefd41d6b83ed3a2
metlx <metlx>
2022-07-08 13:04:27 -0400

    solution for challenge

---
+++ Crash Override.js
@@ -0,0 +1,11 @@
+function aliasGen(a, b) {
+    let fLetter = a[0].toUpperCase()
+    let lLetter = b[0].toUpperCase()
+    if (/^[a-zA-Z]*$/.test(fLetter) && /^[a-zA-Z]*$/.test(lLetter)) {
+        let name = firstName[fLetter]
+        let lame = surname[lLetter]
+        return `${name} ${lame}`
+    } else {
+        return 'Your name must start with a letter from A - Z.'
+    }
+}
